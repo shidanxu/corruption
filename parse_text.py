84afdac7f598168c81aca1a8087ce9f1928419b3
Shidan Xu <shidanxu@mit.edu>
2015-11-21 11:16:19 -0500

    parse text

--- parse_text.py
+++ parse_text.py
@@ -4,7 +4,25 @@
 import os, sys
 import numpy as np
 import urllib, urllib2
+from bosonnlp import BosonNLP
 
+
+def bosonNer(text, sensitivity):
+    nlp = BosonNLP('O8M_j1Nd.4200.wIlhsL46w9-C')
+    return nlp.ner(text, sensitivity)
 
 def parsed_text(original_text):
-    pass
+    result = bosonNer(original_text, 3)
+    # entities is of format [(中方， org_name)]
+    entities = []
+    # print "Type of result is: ", type(result), len(result)
+    for startIndex, endIndex, entityName in result[0]['entity']:
+    	entities.append((''.join(result[0]['word'][startIndex:endIndex]), entityName))
+    
+    # result[0]['word'] = ['中方'， ‘予以’， ‘强烈’， ’谴责‘]
+    # result[0]['entity'] = [[0, 1, org_name]], 
+    # meaning first entry in result[0]['word'] is an org
+    return entities, result[0]['word'], result[0]['entity']
+
+if __name__ == '__main__': 
+	print(parsed_text("中方予以强烈谴责。"))
