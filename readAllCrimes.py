12f1dee8b79cde3a159469f0b2dac61946201f6c
Shidan Xu <shidanxu@mit.edu>
2015-11-22 20:13:15 -0500

    auto check crimes

---
+++ readAllCrimes.py
@@ -0,0 +1,33 @@
+import os
+from sets import Set
+
+crimes = Set()
+punish = Set()
+
+prefix = "./corruption annotated data/"
+for filename in os.listdir("./corruption annotated data/"):
+	if filename.endswith(".ann"):
+		with open(prefix + filename, 'r') as f:
+			lines = f.readlines()
+			for line in lines:
+				values = line.split("\t")
+				if len(values) > 1:
+					tag = values[1]
+					value = values[-1]
+
+					mytag = tag.split(" ")[0].strip()
+					
+					if mytag.endswith("Punish"):
+						punish.add(value)
+					if mytag.endswith("Crime"):
+						crimes.add(value)
+
+with open("crimes_auto.txt", 'w') as f:
+	for item in crimes:
+		f.write(item)
+
+with open("punish_auto.txt", 'w') as f:
+	for item in punish:
+		f.write(item)
+# for item in punish:
+# 	print item
