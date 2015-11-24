0c38086a96d90cf74c52f5ddad436857c5b5f9e4
Shidan Xu <shidanxu@mit.edu>
2015-11-24 09:15:11 -0500

    new evaluation now takes into account length of longest match, and sets. Can now specify the tags to compare

--- evaluation.py
+++ evaluation.py
@@ -1,81 +1,123 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from sets import Set
+import difflib
+
 # This method returns the tags in a file in the format
 # dictionary['Shidan'] = {'Crime': '贪污', 'Punish': '无期徒刑'}
-def process(listtags):
+def process(filename, fields = ['Person', 'Crime', 'Money_Person', 'Punish', 'Position']):
 	dictionary = {}
 	outputDict = {}
-	for line in listtags:
-		tag, tagType, value = line.split("\t")
-		tagType = tagType.split(" ")[0]
-		tagIndex, tagType = int(tagType[:1]), tagType[1:]
+	with open(filename, 'r') as f:
+		listtags = f.readlines()
 
-		if tagIndex in dictionary:
-			if tagType in dictionary[tagIndex]:
-				dictionary[tagIndex][tagType] += (value)
-			else:
-				dictionary[tagIndex][tagType] = value
-		else:
-			dictionary[tagIndex] = {}
-			dictionary[tagIndex][tagType] = value
+		for line in listtags:
+			tag, tagType, value = line.split("\t")
+			tagType = tagType.split(" ")[0]
+			tagIndex, tagType = int(tagType[:1]), tagType[1:]
 
-	for index in dictionary:
-		name = dictionary[index]['Person']
-		outputDict[name] = {}
-		for key in dictionary[index]:
-			if key != 'Person':
-				outputDict[name][key] = dictionary[index][key]
+			if tagType in fields:
+				if tagIndex in dictionary:
+					if tagType in dictionary[tagIndex]:
+						dictionary[tagIndex][tagType].add(value)
+					else:
+						dictionary[tagIndex][tagType] = Set([value])
+				else:
+					dictionary[tagIndex] = {}
+					dictionary[tagIndex][tagType] = Set([value])
+
+		for index in dictionary:
+			name = list(dictionary[index]['Person'])[0]
+			outputDict[name] = {}
+			for key in dictionary[index]:
+				if key != 'Person':
+					outputDict[name][key] = dictionary[index][key]
 
 	return outputDict
 
-def evaluate(human, machine):
+# This baseline specifies whether we want the element or the set element to be compared to
+def maxScore(element, compareSet, baseline):
+	comparisonScores = []
+	for item in compareSet:
+		if baseline == 0:
+			lengthLongest = len(item)
+		else:
+			lengthLongest = len(element)
+
+		s = difflib.SequenceMatcher(None, element, item)
+		m = s.find_longest_match(0, len(element), 0, len(item))
+
+		comparisonScores.append(min(1.0, float(m.size) / lengthLongest))
+
+	# print comparisonScores
+	return max(comparisonScores)
+
+
+
+def evaluate(human, machine, fields = ['Person', 'Crime', 'Money_Person', 'Punish', 'Position']):
 	# Calculate a precision score and a recall score
 	# recall: exact matches in humanDict that are also in machineDict
 	# Precision: line exact matches in machineDict that are also found in humanDict
 	totalScore = 0.0
 	possibleScore = 0.0
-	with open(human, 'r') as f:
-		with open(machine, 'r') as g:
-			humanLabeled = f.readlines()
-			machineLabeled = g.readlines()
-			humanDict = process(humanLabeled)
-			machineDict = process(machineLabeled)
 
-			# print humanDict
-			# for person in humanDict:
-			# 	print person
-			# 	for item in humanDict[person]:
-			# 		print item, humanDict[person][item]
+	humanDict = process(human)
+	machineDict = process(machine)
 
-			for person in humanDict:
-				possibleScore += len(humanDict[person])
-				if person in machineDict:
-					if machineDict[person] == humanDict[person]:
-						totalScore += len(humanDict[person])
-					else:
-						for item in humanDict[person]:
-							if item in machineDict[person]:
-								totalScore += int(humanDict[person][item] == machineDict[person][item])
-			recallScore = totalScore / possibleScore
+	# print humanDict
+	# for person in humanDict:
+	# 	print person
+	# 	for item in humanDict[person]:
+	# 		print item, humanDict[person][item]
 
-			totalScore = 0.0
-			possibleScore = 0.0
+	for person in humanDict:
+		possibleScore += len(humanDict[person])
+		if person in machineDict:
+			if machineDict[person] == humanDict[person]:
+				totalScore += len(humanDict[person])
+			else:
+				for item in humanDict[person]:
+					if item in machineDict[person]:
+						# Here want approximate matching
+						# humanDict[person][item] is a Set
+						total = len(humanDict[person][item])
+						hit = 0.0
+						for element in humanDict[person][item]:
+							hit += maxScore(element, machineDict[person][item], 0)
+						totalScore += hit / total
 
-			for person in machineDict:
-				possibleScore += len(machineDict[person])
-				if person in humanDict:
-					if machineDict[person] == humanDict[person]:
-						totalScore += len(humanDict[person])
-					else:
-						for item in machineDict[person]:
-							if item in humanDict[person]:
-								totalScore += int(humanDict[person][item] == machineDict[person][item])
-			precisionScore = totalScore / possibleScore
+						# totalScore += int(humanDict[person][item] == machineDict[person][item])
+	recallScore = totalScore / possibleScore
+
+	totalScore = 0.0
+	possibleScore = 0.0
+
+	for person in machineDict:
+		possibleScore += len(machineDict[person])
+		if person in humanDict:
+			if machineDict[person] == humanDict[person]:
+				totalScore += len(humanDict[person])
+			else:
+				for item in machineDict[person]:
+					if item in humanDict[person]:
+						total = len(machineDict[person][item])
+						hit = 0.0
+						for element in machineDict[person][item]:
+							hit += maxScore(element, humanDict[person][item], 1)
+
+						totalScore += hit / total
+						# totalScore += int(humanDict[person][item] == machineDict[person][item])
+	precisionScore = totalScore / possibleScore
 
 	return precisionScore, recallScore
 
 if __name__ == '__main__':
 	file1 = "corruption annotated data/L_R_1990_3420.ann"
 	file2 = "corruption annotated data/L_R_1990_3420_mod.ann"
+	# outputDict = process(file1)
+	# for name in outputDict:
+	# 	print name
+	# 	for element in outputDict[name]['Punish']:
+	# 		print element
 	print(evaluate(file1, file2))
