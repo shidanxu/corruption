2d0670ee96ab1e28fdcfd768ce1a3b0d62529eb3
Shidan Xu <shidanxu@mit.edu>
2015-12-12 13:57:34 -0500

    crf_rec

--- crf_rec.py
+++ crf_rec.py
@@ -4,11 +4,23 @@
 import codecs
 import re
 import nltk
-import pycrfsuite
+# import pycrfsuite
 
 def mytrain():
-    mycrftagger = CRFTagger()
+    # mycrftagger = CRFTagger()
+    mytrainingData = read_traindata("L_R_1990_3420.txt.train")
+    print mytrainingData
 
 
-def read_traindata(fname):
-    with open(fname, 'r')
+def read_traindata(fname, path = "./corruption annotated data/"):
+    output = []
+    with codecs.open(path + fname, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+        for line in lines:
+            word, tag = line.split("\t")
+            output.append((word, tag.strip()))
+    return output
+
+
+if __name__ == '__main__':
+    mytrain()
