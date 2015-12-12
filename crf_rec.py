b2b10d5d23adde8b87c6d37df33d47b9a46cad06
tingtao <edmondztt@gmail.com>
2015-12-12 13:52:13 -0500

    save

---
+++ crf_rec.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import codecs
+import re
+import nltk
+import pycrfsuite
+
+def mytrain():
+    mycrftagger = CRFTagger()
+
+
+def read_traindata(fname):
+    with open(fname, 'r')
--- trainCrimeTagFile.py
+++ trainCrimeTagFile.py
@@ -1,125 +1,124 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from BeautifulSoup import BeautifulSoup
 import codecs
 import re
 
 def tagTxt(ftxt, fann, path = './corruption annotated data/'):
-	chars = []
-	alltags = []
-	with codecs.open(path + ftxt, 'r', encoding='utf-8') as txt:
-		with codecs.open(path + fann, 'r', encoding='utf-8') as ann:
-			
-			document = txt.read()
-			soup = BeautifulSoup(document)
-			# print "original encoding: ", soup.originalEncoding
-			for char in document:
-				# print "HAHAHAHAHHAHA", type(char), char
-				chars.append(char)
-				alltags.append(0)
-				# exit(0)
-			print "Length of chars", len(chars)
+    chars = []
+    alltags = []
+    with codecs.open(path + ftxt, 'r', encoding='utf-8') as txt:
+        with codecs.open(path + fann, 'r', encoding='utf-8') as ann:
+
+            document = txt.read()
+            # print "original encoding: ", soup.originalEncoding
+            for char in document:
+                # print "HAHAHAHAHHAHA", type(char), char
+                chars.append(char)
+                alltags.append(0)
+                # exit(0)
+            print "Length of chars", len(chars)
 
 
 
-			annotation = ann.readlines()
-			print "".join(annotation)
+            annotation = ann.readlines()
+            print "".join(annotation)
 
-			for line in annotation:
-				if line:
-					# print line
-					tagLineNumber, tagBulk, tagTxt = line.split("\t")
-					numPlusTag, startIndex, endIndex = tagBulk.split()
-					startIndex = int(startIndex)
-					endIndex = int(endIndex)
-					num = numPlusTag[:1]
-					tag = numPlusTag[1:]
-					# print "line: ", tagLineNumber, tag, startIndex, endIndex, tagTxt
-					for i in range(startIndex, endIndex):
-						# print i
-						char = chars[i]
+            for line in annotation:
+                if line:
+                    # print line
+                    tagLineNumber, tagBulk, tagTxt = line.split("\t")
+                    numPlusTag, startIndex, endIndex = tagBulk.split()
+                    startIndex = int(startIndex)
+                    endIndex = int(endIndex)
+                    num = numPlusTag[:1]
+                    tag = numPlusTag[1:]
+                    # print "line: ", tagLineNumber, tag, startIndex, endIndex, tagTxt
+                    for i in range(startIndex, endIndex):
+                        # print i
+                        char = chars[i]
 
-						if tag == "Crime":
-							alltags[i] = tag
-						# print chars[i]
-						# print tagTxt[i-startIndex]
-	with codecs.open(path + ftxt + ".train", 'w', encoding='utf-8') as writeFile:
-		for char, tag in zip(chars, alltags):
-			# print char, tag
-			tag = str(tag)
-			writeFile.write(char + "\t" + tag + "\n")
-	
-	# Here we use a words approach
-	words = []
-	with codecs.open(path + ftxt, 'r', encoding='utf-8') as txt:
-		words = re.split(r"(\s+)", txt.read())
-		words = [(word, 0, 0, 0) for word in words]
+                        if tag != "Event":
+                            alltags[i] = tag
+                        # print chars[i]
+                        # print tagTxt[i-startIndex]
+    with codecs.open(path + ftxt + ".train", 'w', encoding='utf-8') as writeFile:
+        for char, tag in zip(chars, alltags):
+            # print char, tag
+            tag = str(tag)
+            writeFile.write(char + "\t" + tag + "\n")
 
-		currentLength = 0
-		for ii in range(len(words)):
-			item = words[ii]
-			# print item
-			(word, tag, start, end) = item
-			# print word, len(word)
-			start = currentLength
-			currentLength += len(word)
-			end = currentLength
+    # Here we use a words approach
+    words = []
+    with codecs.open(path + ftxt, 'r', encoding='utf-8') as txt:
+        words = re.split(r"(\s+)", txt.read())
+        words = [(word, 0, 0, 0) for word in words]
 
-			words[ii] = (word, tag, start, end)
+        currentLength = 0
+        for ii in range(len(words)):
+            item = words[ii]
+            # print item
+            (word, tag, start, end) = item
+            # print word, len(word)
+            start = currentLength
+            currentLength += len(word)
+            end = currentLength
 
-		print len(words), currentLength
-		print words
+            words[ii] = (word, tag, start, end)
 
-	with codecs.open(path + fann, 'r', encoding='utf-8') as ann:
-		annotation = ann.readlines()
-		print "".join(annotation)
+        print len(words), currentLength
+        print words
 
-		for line in annotation:
-			if line:
-				# print line
-				tagLineNumber, tagBulk, tagTxt = line.split("\t")
-				numPlusTag, startIndex, endIndex = tagBulk.split()
-				startIndex = int(startIndex)
-				endIndex = int(endIndex)
-				num = numPlusTag[:1]
-				tag = numPlusTag[1:]
-				# print "line: ", tagLineNumber, tag, startIndex, endIndex, tagTxt
-				
-				if tag == "Crime":
-					startFound = False
-					endFound = False
-					print "Found crime: ", ''.join(chars[startIndex : endIndex])
-					print startIndex, endIndex
-					for ii in range(len(words)):
-						(word, tag, start, end) = words[ii]
-						
-						if not startFound:
-							# print "start, end, startIndex, endIndex:", start, end, startIndex, endIndex
-							if startIndex < end:
-								tag = "Crime"
-								startFound = True
-							words[ii] = (word, tag, start, end)
-							continue
+    with codecs.open(path + fann, 'r', encoding='utf-8') as ann:
+        annotation = ann.readlines()
+        print "".join(annotation)
 
-						if startFound:
-							print "Start found, ", start, end, startIndex, endIndex
-							tag = "Crime"
-							if endIndex <= start:
-								endFound = True
-								break
-							words[ii] = (word, tag, start, end)
-							
-								
+        for line in annotation:
+            if line:
+                # print line
+                tagLineNumber, tagBulk, tagTxt = line.split("\t")
+                numPlusTag, startIndex, endIndex = tagBulk.split()
+                startIndex = int(startIndex)
+                endIndex = int(endIndex)
+                num = numPlusTag[:1]
+                tagAnn = numPlusTag[1:]
+                # print "line: ", tagLineNumber, tag, startIndex, endIndex, tagTxt
 
-	print words
-	with codecs.open(path + ftxt + ".train", 'w', encoding='utf-8') as writeFile:
-		for (word, tag, start, end) in words:
-			if word.split():
-				tag = str(tag)
-				writeFile.write(word + "\t" + tag + "\n")
-	
+                # if tag == "Crime":
+                if tagAnn != "Event":
+                    startFound = False
+                    endFound = False
+                    print "Found crime: ", ''.join(chars[startIndex : endIndex])
+                    print startIndex, endIndex
+                    for ii in range(len(words)):
+                        (word, tag, start, end) = words[ii]
+
+                        if not startFound:
+                            # print "start, end, startIndex, endIndex:", start, end, startIndex, endIndex
+                            if startIndex < end:
+                                tag = tagAnn
+                                startFound = True
+                            words[ii] = (word, tag, start, end)
+                            continue
+
+                        if startFound:
+                            print "Start found, ", start, end, startIndex, endIndex
+                            tag = tagAnn
+                            if endIndex <= start:
+                                endFound = True
+                                break
+                            words[ii] = (word, tag, start, end)
+
+
+
+    print words
+    with codecs.open(path + ftxt + ".train", 'w', encoding='utf-8') as writeFile:
+        for (word, tag, start, end) in words:
+            if word.split():
+                tag = str(tag)
+                writeFile.write(word + "\t" + tag + "\n")
+
 
 
 if __name__ == '__main__':
-	tagTxt("L_R_1990_3399.txt", "L_R_1990_3399.ann")
+    tagTxt("L_R_1990_3420.txt", "L_R_1990_3420.ann")
